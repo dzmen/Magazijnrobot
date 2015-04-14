33afa0217bca9f3170ebc7ac806eb2df08a42597
Timmerman H <Hugo.t.95@hotmail.com>
2015-04-14 11:12:28 +0200

    Edit

--- BPP-Simuart/BPPsimulator/src/GUI/Scherm.java
+++ BPP-Simuart/BPPsimulator/src/GUI/Scherm.java
@@ -11,7 +11,9 @@ import java.awt.ScrollPane;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  *
@@ -19,24 +21,54 @@ import javax.swing.JPanel;
  */
 public class Scherm extends JFrame {
 
-    Tekenpanel tekenpaneel = new Tekenpanel();
-    JButton addXML = new JButton("selecteer bestand");
-    JButton ExeProgram = new JButton("Start simulatie");
-    JComboBox Algoritme = new JComboBox();
-    Statscherm stats;
-    ScrollPane ap;
+    private Tekenpanel tekenpaneel;
+    private JComboBox selectiemenu;
+    private JLabel jcomp3;
+    private JLabel jcomp4;
+    private JButton xmlSelect;
+    private JTextArea jcomp6;
+    private JLabel jcomp7;
+    private JButton programeExe;
 
     public Scherm() {
-        
+
         this.setTitle("BPP-simulator");
         this.setSize(820, 600);
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
+
+        String[] jcomp2Items = {"Numerieke Algoritme", "Gretige Algoritme", "Balans Algoritme"};
+
+        //construct components
+        tekenpaneel = new Tekenpanel();
+        selectiemenu = new JComboBox(jcomp2Items);
+        jcomp3 = new JLabel("Algoritme");
+        jcomp4 = new JLabel("XML-Bestand");
+        xmlSelect = new JButton("selecteer bestand...");
+        jcomp6 = new JTextArea(5, 5);
+        jcomp7 = new JLabel("newLabel");
+        programeExe = new JButton("Execute");
+
         this.setLayout(null);
-        this.add(tekenpaneel);
-        stats = new Statscherm();
-        ap.setBounds(30, 20, 500, 400);
-        ap = new ScrollPane(stats);
+        //set component bounds (only needed by Absolute Positioning)
+        tekenpaneel.setBounds(30, 20, 500, 400);
+        selectiemenu.setBounds(575, 85, 155, 25);
+        jcomp3.setBounds(575, 50, 100, 25);
+        jcomp4.setBounds(575, 190, 100, 25);
+        xmlSelect.setBounds(570, 215, 155, 30);
+        jcomp6.setBounds(30, 435, 315, 105);
+        jcomp7.setBounds(360, 435, 260, 105);
+        programeExe.setBounds(570, 300, 100, 25);
+        //add components
+        add(tekenpaneel);
+        add(selectiemenu);
+        add(jcomp3);
+        add(jcomp4);
+        add(xmlSelect);
+        add(jcomp6);
+        add(jcomp7);
+        add(programeExe);
+
     }
 }
