95c35fb4329a7c82cbd9d8c71f7b17951da6a59b
Vroomshoop <robbin-geerlings@hotmail.com>
2015-04-21 14:51:42 +0200

    algoritme 3 update

--- BPP-Simuart/BPP-simulatorV2/src/Items/Algoritme3.java
+++ BPP-Simuart/BPP-simulatorV2/src/Items/Algoritme3.java
@@ -17,47 +17,50 @@ public class Algoritme3 extends TemplateAlgoritme {
     @Override
     public void runAlgoritme(ArrayList<Doos> boxes, ArrayList<Pakket> packets) {
         //Voer het algoritme uit met gebruik van boxes en packets.
-
+        
+        
+        ArrayList<Pakket> newpackets = new ArrayList<Pakket>();
+        int f = boxes.size();
+        int g = 0;
+        int h = packets.size();
+        int i;
+        int j;
+        Pakket low = new Pakket();
+        while(packets.size()>0){
+            low.setSize(1.0);
+            i = 0;
+            j = 0;
+            for(Pakket ab: packets){
+                if(ab.getSize()<low.getSize()){
+                    low = ab;
+                    j=i;
+                }
+                i++;
+            }
+            newpackets.add(low);
+            packets.remove(j);
+        }
+        packets = newpackets;
         int a = boxes.size();
         int b = 0;
         int c = packets.size();
-        int d;
-        int current = 0;
-        double lowest = 1;
-        ArrayList<Pakket> newpackets = new ArrayList<Pakket>();
-        while (packets.size() > 0) {
-            for (d = 0; d < c; d++) {
-                if (packets.get(d).getSize() > lowest) {
-                } else {
-                    lowest = packets.get(d).getSize();
-                    current = d;
-                }
-            }
-            newpackets.add(packets.get(d));
-            packets.remove(d);
-        }
-         a = boxes.size();
-         b = 0;
-         c = newpackets.size();
-         d = 0;
-         double corrent = 0.0;
-
+        int d = 0;
+        double current = 0;
         while (b < a && d < c) {
-            if (packets.get(d).getSize() + corrent > 1) {
+            if (packets.get(d).getSize() + current > 1) {
                 b++;
-
-                boxes.get(b).addPakket(newpackets.get(d));
-                corrent = newpackets.get(d).getSize();
+                
+                boxes.get(b).addPakket(packets.get(d));
+                current = packets.get(d).getSize();
                 d++;
-
+                
             } else {
-                boxes.get(b).addPakket(newpackets.get(d));
+                boxes.get(b).addPakket(packets.get(d));
 
-                corrent = current + newpackets.get(d).getSize();
+                current = current + packets.get(d).getSize();
                 d++;
             }
         }
-
         //verander de array (zie TemplateAlgoritme
         dozen = boxes;
     }
